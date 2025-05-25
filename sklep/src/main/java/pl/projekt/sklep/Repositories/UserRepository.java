e474971a14aa7f723c2352da57f5bb7e67ed6ec3
Arnamis <bartoszck@gmail.com>
2025-05-25 17:19:28 +0200

    Delete sklep/src/main/java/pl/projekt/sklep/Repositories/UserRepository.java

--- sklep/src/main/java/pl/projekt/sklep/Repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.projekt.sklep.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.projekt.sklep.Models.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByFirstNameAndLastName(String firstName, String lastName);
-}
